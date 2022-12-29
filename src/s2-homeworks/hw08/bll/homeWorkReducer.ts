35186265b371494d7a64b172f4740182510c93f0
yanakorobova <yanaheaven@yandex.ru>
2022-12-29 15:42:48 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const newState = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1
+                if (a.name < b.name) return -1
+                else return 0
+            })
+            // need to fix
+            return action.payload === "up" ? newState : newState.reverse()
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload) // need to fix
         }
         default:
             return state
